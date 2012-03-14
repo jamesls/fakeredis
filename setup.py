54c70e11982adb0dbe3f58d1755ac989648f2a7f
James Saryerwinnie <jls.npi@gmail.com>
2012-03-14 15:09:23 -0700

    Fix bug in setup.py so I can upload to pypi

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ setup(
     version='0.1',
     description="Fake implementation of redis API for testing purposes.",
     license='BSD',
+    url="https://github.com/jamesls/fakeredis",
     author='James Saryerwinnie',
     author_email='jlsnpi@gmail.com',
     py_modules=['fakeredis'],
     classifiers=[
-        'Development Status :: 3 - Alpha'
+        'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: BSD License',
     ],
     install_requires=[
