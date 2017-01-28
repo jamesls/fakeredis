c8dd5c0c9e64a8fd6a2af41a580ec79eacc91679
Jon Dufresne <jon.dufresne@gmail.com>
2017-01-28 09:39:39 -0800

    Add Python 3.6 to Travis CI and document in setup.py
    Alphabetized classifiers list.

--- setup.py
+++ setup.py
@@ -16,14 +16,17 @@ setup(
     py_modules=['fakeredis'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     install_requires=[
         'redis',
     ]
 )
-
