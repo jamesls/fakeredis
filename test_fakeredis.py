91b8a62007f582426ce6b1cbf5c5a9f3ce77b44b
Konstantinos Koukopoulos <kouk@transifex.com>
2018-02-01 14:56:40 +0200

    Support py-redis 2.10.6
    In 2.10.6 the SET command will pass a zero expiration directly to the
    server, which returns an error. Previously it ignored it.
    
    https://github.com/andymccurdy/redis-py/blob/d48c46d3f8c6943c7995bffbecb0fb4c66daabe2/CHANGES#L8

--- test_fakeredis.py
+++ test_fakeredis.py
@@ -2625,7 +2625,7 @@ class TestFakeRedis(unittest.TestCase):
 
     @attr('slow')
     def test_set_ex_should_expire_value(self):
-        self.redis.set('foo', 'bar', ex=0)
+        self.redis.set('foo', 'bar')
         self.assertEqual(self.redis.get('foo'), b'bar')
         self.redis.set('foo', 'bar', ex=1)
         sleep(2)
