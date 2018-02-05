da8a82035a61c69e16c67f06bb00b73926bf17ba
Blake Foster <bfoster@amplify.com>
2018-02-05 14:16:26 -0500

    Bump patch version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fakeredis',
-    version='0.9.0',
+    version='0.9.1',
     description="Fake implementation of redis API for testing purposes.",
     long_description=open(os.path.join(os.path.dirname(__file__),
                                        'README.rst')).read(),
