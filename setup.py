66e9194b8e10595992280637d9941205101ffe75
Bruce Merry <bmerry@ska.ac.za>
2019-01-14 09:52:20 +0200

    Bump version to 1.0rc1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='fakeredis',
-    version='1.0b1',
+    version='1.0rc1',
     description="Fake implementation of redis API for testing purposes.",
     long_description=open(os.path.join(os.path.dirname(__file__),
                                        'README.rst')).read(),
