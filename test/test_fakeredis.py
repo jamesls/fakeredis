ba404d8619849b6c97180f0a49b94b83a6797ccc
mgetka <michal.getka@gmail.com>
2020-08-14 16:33:20 +0200

    Fix styling in SET w/ KEEPTTL tests

--- test/test_fakeredis.py
+++ test/test_fakeredis.py
@@ -678,9 +678,11 @@ def test_set_keepttl(r):
     assert r.ttl('foo') == 100
     assert r.get('foo') == b'baz'
 
+
 def test_set_conflicting_expire_options(r):
     with pytest.raises(ResponseError):
         r.set('foo', 'bar', ex=1, px=1)
+
 
 @pytest.mark.skipif(REDIS_VERSION < '3.5', reason="Test is only applicable to redis-py 3.5+")
 def test_set_conflicting_expire_options_w_keepttl(r):
@@ -690,6 +692,7 @@ def test_set_conflicting_expire_options_w_keepttl(r):
         r.set('foo', 'bar', px=1, keepttl=True)
     with pytest.raises(ResponseError):
         r.set('foo', 'bar', ex=1, px=1, keepttl=True)
+
 
 def test_set_raises_wrong_ex(r):
     with pytest.raises(ResponseError):
