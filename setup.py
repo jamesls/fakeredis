c1f68d25b595b18d1f55c06441efbd2f4cf74b51
Bruce Merry <bmerry@ska.ac.za>
2020-11-29 14:05:30 +0200

    Stop testing on Python 3.5
    It's EOL, and the latest hypothesis requires 3.6.

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8'
