541c91a3f55728015d1a49f25a055b5271e61b5c
Bruce Merry <bmerry@ska.ac.za>
2020-11-29 15:19:50 +0200

    Test against Python 3.9

--- setup.py
+++ setup.py
@@ -22,7 +22,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
     ],
     python_requires='>=3.5',
     install_requires=[
