54843796aa0f2af27482dbc75c2875c7ccf37ad4
Bruce Merry <bmerry@ska.ac.za>
2021-08-16 12:02:59 +0200

    Some packaging modernisation
    - Add a pyproject.toml file (with setuptools_scm, so that pypa-build
      will include everything from SCM in the build; and remove MANIFEST.in)
    - Move all the metadata from setup.py to setup.cfg (setup.py is left
      just to support editable builds).
    - Use `attr:` in setup.cfg to get the version programmatically instead
      of repeating it.
    - Update my email address

--- setup.py
+++ setup.py
@@ -1,39 +1,4 @@
-import os
-
 from setuptools import setup
 
 
-setup(
-    name='fakeredis',
-    version='1.6.0',
-    description="Fake implementation of redis API for testing purposes.",
-    long_description=open(os.path.join(os.path.dirname(__file__),
-                                       'README.rst')).read(),
-    license='BSD',
-    url="https://github.com/jamesls/fakeredis",
-    author='James Saryerwinnie',
-    author_email='js@jamesls.com',
-    maintainer='Bruce Merry',
-    maintainer_email='bmerry@ska.ac.za',
-    packages=['fakeredis'],
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9'
-    ],
-    python_requires='>=3.5',
-    install_requires=[
-        # Minor version updates to redis tend to break fakeredis. If you
-        # need to use fakeredis with a newer redis, please submit a PR that
-        # relaxes this restriction and adds it to the Github Actions tests.
-        'redis<3.6.0', 'six>=1.12', 'sortedcontainers'
-    ],
-    extras_require={
-        'lua': ['lupa'],
-        'aioredis': ['aioredis']
-    }
-)
+setup()
